9d0fa4858fc7e16b9e03ecbc5dfdb8bed5a60221
Dong-woo Kang <84041490+Autumn-Rainfall@users.noreply.github.com>
2023-04-03 00:57:32 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './screen/Main';
-import Login from './auth/login/Login';
-import SignUp from './auth/signUp/SignUp';
-import Find from './auth/find/Find';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./screen/Main";
+import Login from "./auth/login/Login";
+import SignUp from "./auth/signUp/SignUp";
+import Find from "./auth/find/Find";
 import MyInfo from "./myInfo/MyInfo";
-import './app.css';
+import Redirect from "./auth/Redirect/Redirect";
+import "./app.css";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={'/'} element={<Main />}></Route>
+        <Route path={"/"} element={<Main />}></Route>
         <Route path={"/myInfo"} element={<MyInfo />}></Route>
-        <Route path={'/login'} element={<Login />}></Route>
-        <Route path={'/signUp'} element={<SignUp />}></Route>
-        <Route path={'/find'} element={<Find />}></Route>
-        <Route path={'/redirect'} element={<Redirect />}></Route>
+        <Route path={"/login"} element={<Login />}></Route>
+        <Route path={"/signUp"} element={<SignUp />}></Route>
+        <Route path={"/find"} element={<Find />}></Route>
+        <Route path={"/redirect"} element={<Redirect />}></Route>
       </Routes>
     </BrowserRouter>
   );
