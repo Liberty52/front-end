2079d7a939b0d0131da0d1e4565931c0fd4d1d1f
2hanbyeol1 <2hanbyeol1@naver.com>
2023-04-03 00:15:38 +0900

    axios code separate & login api

--- src/auth/signUp/SignUp.js
+++ src/auth/signUp/SignUp.js
@@ -1,36 +1,10 @@
 import './SignUp.css';
+import post from '../../axios/auth/SignUp.js';
 import InputGroup from '../../common/InputGroup';
 import Button from '../../common/Button';
 import photo from '../../image/icon/photo.png';
 import logo from '../../image/icon/logo.png';
 import { useState } from 'react';
-import axios from 'axios';
-
-function post(dto, file) {
-  const formData = new FormData();
-  formData.append('file', file);
-  formData.append(
-    'dto',
-    new Blob([JSON.stringify(dto)], { type: 'application/json' })
-  );
-  axios
-    .post('http://localhost:8080/auth/sign-up', formData, {
-      headers: {
-        'Contest-Type': 'multipart/form-data',
-      },
-    })
-    .then(() => {
-      alert(dto.name + '님 회원 가입을 환영합니다.');
-      // window.location.replace('/login')
-    })
-    .catch(e => {
-      if (e.response) {
-        if (e.response.status === 500) {
-          alert(dto.email + '은 사용할 수 없는 이메일입니다.');
-        }
-      }
-    });
-}
 
 function SignUpForm() {
   return (
