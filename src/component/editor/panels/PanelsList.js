1a34442f8dc82544ae5cd6d9614394f03b1c1774
NEM-NE <dlatqdlatq@mju.ac.kr>
2023-04-16 01:08:07 +0900

    Impl Panels

---
+++ src/component/editor/panels/PanelsList.js
@@ -0,0 +1,84 @@
+import { useStyletron, styled } from "baseui"
+import Icons from "../icons"
+import { Block } from "baseui/block"
+import { useTranslation } from "react-i18next"
+import useAppContext from "../../../hooks/useAppContext"
+import Scrollable from "../Scrollable"
+import useSidebarOpen from "../../../hooks/useSidebarOpen"
+
+const Container = styled("div", (props) => ({
+  width: "80px",
+  backgroundColor: props.$theme.colors.primary100,
+  display: "flex",
+  flex: "none",
+}))
+
+const BASE_ITEMS = [
+    {
+      id: "images",
+      name: "Images",
+    },
+  ]
+
+const PanelsList = () => {
+  const { activePanel } = useAppContext()
+  const { t } = useTranslation("editor")
+  const PANEL_ITEMS = BASE_ITEMS
+  return (
+    <Container>
+      <Scrollable autoHide={true}>
+        {PANEL_ITEMS.map((panelListItem) => (
+          <PanelListItem
+            label={panelListItem.name}
+            name={panelListItem.name}
+            key={panelListItem.name}
+            icon={panelListItem.name}
+            activePanel={activePanel}
+          />
+        ))}
+      </Scrollable>
+    </Container>
+  )
+}
+
+const PanelListItem = ({ label, icon, activePanel, name }) => {
+  const { setActivePanel } = useAppContext()
+  const {setIsSidebarOpen} = useSidebarOpen()
+  const [css, theme] = useStyletron()
+  // @ts-ignore
+  const Icon = Icons[icon]
+  return (
+    <Block
+      id="EditorPanelList"
+      onClick={() => {
+        setIsSidebarOpen(true)
+        setActivePanel(name)
+      }}
+      $style={{
+        width: "80px",
+        height: "80px",
+        backgroundColor: name === activePanel ? theme.colors.white : theme.colors.primary100,
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "center",
+        fontFamily: "Poppins",
+        fontWeight: 500,
+        fontSize: "0.8rem",
+        userSelect: "none",
+        transition: "all 0.5s",
+        gap: "0.1rem",
+        ":hover": {
+          cursor: "pointer",
+          backgroundColor: theme.colors.white,
+          transition: "all 1s",
+        },
+      }}
+    >
+      <Icon size={24} />
+      <div>{label}</div>
+    </Block>
+  )
+}
+
+export default PanelsList
---
+++ src/component/editor/panels/panelItems/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+// import Customize from "./Customize"
+// import Templates from "./Templates"
+// import Uploads from "./Uploads"
+import Images from "./Images"
+// import Layers from "./Layers"
+export default {
+//   Customize: Customize,
+//   Templates: Templates,
+//   Uploads: Uploads,
+  Images: Images,
+//   Layers: Layers,
+}
