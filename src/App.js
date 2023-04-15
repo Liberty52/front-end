0c79985e11edc54d9af3fe2acd6f13a18bb213c6
NEM-NE <dlatqdlatq@mju.ac.kr>
2023-04-16 01:06:20 +0900

    Add provider

--- src/App.js
+++ src/App.js
@@ -9,10 +9,13 @@ import Cart from './screen/shopping/cart/Cart';
 import Order from './screen/product/Order';
 import ChangePasswordPage from './screen/auth/redirect/ChangePasswordPage';
 import './app.css';
+import Editor from './screen/product/Editor';
+import Provider from './Provider';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <Provider>
+      <BrowserRouter>
       <Routes>
         <Route path={'/'} element={<Main />}></Route>
         <Route path={'/myInfo'} element={<MyInfo />}></Route>
@@ -22,8 +25,11 @@ export default function App() {
         <Route path={'/purchase'} element={<Purchase />}></Route>
         <Route path={'/cart'} element={<Cart />}></Route>
         <Route path={"/product/order"} element={<Order />}></Route>
+        <Route path={'/product_speaker/:id'} element={<Order />}></Route>
+        <Route path={'/product_speaker/:id/edit'} element={<Editor />} />
         <Route path={'/auth/password'} element={<ChangePasswordPage />} />
       </Routes>
     </BrowserRouter>
+    </Provider>
   );
 }
---
+++ src/contexts/DesignEditor.js
@@ -0,0 +1,109 @@
+import React from "react"
+
+export const DesignEditorContext = React.createContext({
+  scenes: [],
+  setScenes: () => {},
+  currentScene: null,
+  setCurrentScene: () => {},
+  currentDesign: {
+    id: "",
+    frame: {
+      width: 1,
+      height: 1,
+    },
+    metadata: {},
+    name: "",
+    preview: "",
+    scenes: [],
+    type: "",
+  },
+  setCurrentDesign: () => {},
+  isSidebarOpen: true,
+  setIsSidebarOpen: () => {},
+  editorType: "NONE",
+  setEditorType: () => {},
+  displayPlayback: false,
+  setDisplayPlayback: () => {},
+  displayPreview: false,
+  setDisplayPreview: () => {},
+  currentPreview: "",
+  setCurrentPreview: () => {},
+  maxTime: 0,
+  setMaxTime: () => {},
+  contextMenuTimelineRequest: {
+    id: "",
+    left: 0,
+    top: 0,
+    visible: false,
+  },
+  setContextMenuTimelineRequest: () => {},
+  contextMenuSceneRequest: {
+    id: "",
+    left: 0,
+    top: 0,
+    visible: false,
+  },
+  setContextMenuSceneRequest: () => {},
+
+})
+
+export const DesignEditorProvider = ({ children }) => {
+  const [scenes, setScenes] = React.useState([])
+  const [currentScene, setCurrentScene] = React.useState(null)
+  const [currentDesign, setCurrentDesign] = React.useState({
+    id: "",
+    frame: {
+      width: 1,
+      height: 1,
+    },
+    metadata: {},
+    name: "",
+    preview: "",
+    scenes: [],
+    type: "",
+  })
+  const [isSidebarOpen, setIsSidebarOpen] = React.useState(true)
+  const [editorType, setEditorType] = React.useState("NONE")
+  const [displayPlayback, setDisplayPlayback] = React.useState(false)
+  const [displayPreview, setDisplayPreview] = React.useState(false)
+  const [currentPreview, setCurrentPreview] = React.useState("")
+  const [maxTime, setMaxTime] = React.useState(5000)
+  const [contextMenuTimelineRequest, setContextMenuTimelineRequest] = React.useState({
+    id: "",
+    left: 0,
+    top: 0,
+    visible: false,
+  })
+  const [contextMenuSceneRequest, setContextMenuSceneRequest] = React.useState({
+    id: "",
+    left: 0,
+    top: 0,
+    visible: false,
+  })
+  const context = {
+    scenes,
+    setScenes,
+    currentScene,
+    setCurrentScene,
+    currentDesign,
+    setCurrentDesign,
+    isSidebarOpen,
+    setIsSidebarOpen,
+    editorType,
+    setEditorType,
+    displayPlayback,
+    setDisplayPlayback,
+    displayPreview,
+    setDisplayPreview,
+    currentPreview,
+    setCurrentPreview,
+    maxTime,
+    setMaxTime,
+    contextMenuTimelineRequest,
+    setContextMenuTimelineRequest,
+    contextMenuSceneRequest,
+    setContextMenuSceneRequest,
+  }
+  
+  return <DesignEditorContext.Provider value={context}>{children}</DesignEditorContext.Provider>
+}
