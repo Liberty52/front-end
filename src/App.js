f6237048fb7f4a7c244f0c494ac8e8d8668bf508
동우 <kanggo00@gmail.com>
2023-04-17 04:04:01 +0900

    Conflicting files

--- src/App.js
+++ src/App.js
@@ -4,26 +4,33 @@ import Login from "./screen/auth/login/Login";
 import SignUp from "./screen/auth/signUp/SignUp";
 import MyInfo from "./screen/auth/myInfo/MyInfo";
 import Redirect from "./screen/auth/redirect/Redirect";
-import Cart from "./screen/cart/Cart";
+import Purchase from "./screen/shopping/purchase/Purchase";
+import Cart from "./screen/shopping/cart/Cart";
 import Order from "./screen/product/Order";
 import Payment from "./screen/product/payment/Payment";
 import ChangePasswordPage from "./screen/auth/redirect/ChangePasswordPage";
+import Inquiry from "./screen/shopping/inquiry/Inquiry";
+import Detail from "./screen/shopping/detail/Detail";
 import "./app.css";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<Main />}></Route>
-        <Route path={"/myInfo"} element={<MyInfo />}></Route>
-        <Route path={"/login"} element={<Login />}></Route>
-        <Route path={"/signUp"} element={<SignUp />}></Route>
-        <Route path={"/redirect"} element={<Redirect />}></Route>
-        <Route path={"/cart"} element={<Cart />}></Route>
-        <Route path={"/payment"} element={<Payment />}></Route>
-        <Route path={"/order"} element={<Order />}></Route>
-        <Route path={"/auth/password"} element={<ChangePasswordPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Main />}></Route>
+          <Route path={"/myInfo"} element={<MyInfo />}></Route>
+          <Route path={"/login"} element={<Login />}></Route>
+          <Route path={"/signUp"} element={<SignUp />}></Route>
+          <Route path={"/redirect"} element={<Redirect />}></Route>
+          <Route path={"/purchase"} element={<Purchase />}></Route>
+          <Route path={"/cart"} element={<Cart />}></Route>
+          <Route path={"/product_speaker/:id"} element={<Order />}></Route>
+          <Route path={"/auth/password"} element={<ChangePasswordPage />} />
+          <Route path={"/inquiry"} element={<Inquiry />} />
+          <Route path={"/detail/:orderId"} element={<Detail />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
---
+++ src/axios/shopping/Purchase.js
@@ -0,0 +1,20 @@
+import axios from "../axios";
+import Select from "react-select";
+
+export default function post(dto) {
+  const formData = new FormData();
+  formData.append(
+    "dto",
+    new Blob([JSON.stringify(dto)], { type: "application/json" })
+  );
+  axios
+    .post("/product/cart-items", formData, {
+      headers: {
+        "Contest-Type": "multipart/form-data",
+      },
+    })
+    .then(() => {
+      alert("장바구니에 담겼습니다");
+      window.location.replace("/");
+    });
+}
