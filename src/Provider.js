fa316b641ecae4ca3ff80a4680d2fbf5d3bead72
NEM-NE <dlatqdlatq@mju.ac.kr>
2023-04-17 18:06:41 +0900

    Fix provider wrapper

--- src/Provider.js
+++ src/Provider.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Client as Styletron } from "styletron-engine-atomic"
 import { Provider as StyletronProvider } from "styletron-react"
-import { BaseProvider, LightTheme } from "baseui"
 import { AppProvider } from "./contexts/AppContext"
 import {DesignEditorProvider} from "./contexts/DesignEditor.js"
 // import "./translations"
@@ -10,15 +9,15 @@ const engine = new Styletron()
 
 const Provider = ({ children }) => {
   return (
-      <DesignEditorProvider>
-          <AppProvider>
-              <StyletronProvider value={engine}>
-                <BaseProvider theme={LightTheme}>
-                {children}
-                </BaseProvider>
-              </StyletronProvider>
-          </AppProvider>
-      </DesignEditorProvider>
+    <React.Fragment>
+        <DesignEditorProvider>
+            <AppProvider>
+                <StyletronProvider value={engine}>
+                  {children}
+                </StyletronProvider>
+            </AppProvider>
+        </DesignEditorProvider>
+      </React.Fragment>
   )
 }
 
