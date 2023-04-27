6a0dcd84572e2a3f635a36bbc7cf89a308abf733
NEM-NE <dlatqdlatq@mju.ac.kr>
2023-04-27 16:35:08 +0900

    Add limit when user uploads image

--- src/screen/product/Editor.js
+++ src/screen/product/Editor.js
@@ -5,21 +5,24 @@ import Canvas from '../../component/editor/canvas/Canvas';
 import { BaseProvider, LightTheme } from "baseui"
 import Toolbox from '../../component/editor/toolbax';
 import Footer from '../../component/editor/Footer';
+import { ToasterContainer, PLACEMENT } from 'baseui/toast';
 
 
 const Editor = () => {
     return (
       <BaseProvider theme={LightTheme}>
-          <EditorContainer>
-            <div style={{ display: "flex", flex: 1 }}>
-              <Panels/>
-              <div style={{ flex: 1, display: "flex", flexDirection: "column", position: "relative" }}>
-                <Toolbox />
-                <Canvas />
-                <Footer />
+          <ToasterContainer placement={PLACEMENT.bottomRight}>
+            <EditorContainer>
+              <div style={{ display: "flex", flex: 1 }}>
+                <Panels/>
+                <div style={{ flex: 1, display: "flex", flexDirection: "column", position: "relative" }}>
+                  <Toolbox />
+                  <Canvas />
+                  <Footer />
+                </div>
               </div>
-            </div>
-          </EditorContainer>
+            </EditorContainer>
+          </ToasterContainer>
         </BaseProvider>
       );
 }
