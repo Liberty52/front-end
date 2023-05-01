9e0957894587a9714c8c435ed6d8e0bb33a9aff1
2hanbyeol1 <2hanbyeol1@naver.com>
2023-05-01 16:09:36 +0900

    소셜 로그인 버튼 디자인 변경

--- src/global/Constants.js
+++ src/global/Constants.js
@@ -1,32 +1,33 @@
 import { SOCIAL_LOGIN_URL } from './url';
+import facebookLogo from '../image/icon/facebook-logo.png';
 
 const SOCIAL_LOGIN_PROVIDER = {
   NAVER: {
-    text: "네이버 로그인",
+    img: 'https://vendor-cdn.imweb.me/images/naver_login2x.png',
     url: SOCIAL_LOGIN_URL.NAVER,
-    css: "naver",
+    css: 'naver',
   },
   KAKAO: {
-    text: "카카오 로그인",
+    img: 'https://vendor-cdn.imweb.me/images/kakao_icon.png',
     url: SOCIAL_LOGIN_URL.KAKAO,
-    css: "kakao",
+    css: 'kakao',
   },
   GOOGLE: {
-    text: "구글 로그인",
+    img: 'https://vendor-cdn.imweb.me/images/google_icon.png',
     url: SOCIAL_LOGIN_URL.GOOGLE,
-    css: "google",
+    css: 'google',
   },
   FACEBOOK: {
-    text: "페이스북 로그인",
+    img: facebookLogo,
     url: SOCIAL_LOGIN_URL.FACEBOOK,
-    css: "facebook",
+    css: 'facebook',
   },
 };
 
 const HTML_EDITOR_MODE = {
-  ADD : "ADD",
-  UPDATE : "UPDATE"
-}
+  ADD: 'ADD',
+  UPDATE: 'UPDATE',
+};
 
 export { SOCIAL_LOGIN_PROVIDER, HTML_EDITOR_MODE };
 export const API_LOGIN_GOOGLE = `${SOCIAL_LOGIN_URL}/google`;
