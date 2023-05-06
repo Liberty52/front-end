5eac30e0374c11a31d68cd72df6156c30ef3cd79
동우 <kanggo00@gmail.com>
2023-05-06 20:27:05 +0900

    장바구니 결제 및 비회원 결제

--- src/axios/shopping/Payment.js
+++ src/axios/shopping/Payment.js
@@ -1,73 +1,196 @@
 import axios from "../axios";
 
 export function prepareCard(dto, file) {
-    const formData = new FormData();
-    formData.append('imageFile', file);
-    formData.append(
-        'dto',
-        new Blob([JSON.stringify(dto)], { type: 'application/json' })
-    );
-    return new Promise(res => {
-        axios
-            .post('/product/orders/payment/card/prepare',
-                formData,
-                {
-                    headers: {
-                        'Content-Type': `multipart/form-data`,
-                        'Authorization': localStorage.getItem('ACCESS_TOKEN'),
-                    }
-                }
-            )
-            .then(response => {
-                res(response.data);
-            })
-            .catch(e => {
-                console.log(e);
-                alert("카드결제 요청에 실패했습니다.");
-            })
+  const formData = new FormData();
+  formData.append("imageFile", file);
+  formData.append(
+    "dto",
+    new Blob([JSON.stringify(dto)], { type: "application/json" })
+  );
+  if (localStorage.get("ACCESS_TOKEN")) {
+    return new Promise((res) => {
+      axios
+        .post("/product/orders/payment/card/prepare", formData, {
+          headers: {
+            "Content-Type": `multipart/form-data`,
+            Authorization: localStorage.getItem("ACCESS_TOKEN"),
+          },
+        })
+        .then((response) => {
+          res(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+          alert("카드결제 요청에 실패했습니다.");
+        });
     });
+  } else {
+    return new Promise((res) => {
+      axios
+        .post("/product/guest/orders/payment/card/prepare", formData, {
+          headers: {
+            "Content-Type": `multipart/form-data`,
+            Authorization: formData.destinationDto.receiverPhoneNumber,
+          },
+        })
+        .then((response) => {
+          res(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+          alert("카드결제 요청에 실패했습니다.");
+        });
+    });
+  }
+}
+
+export function prepareCardCart(dto) {
+  const formData = new FormData();
+  formData.append(
+    "dto",
+    new Blob([JSON.stringify(dto)], { type: "application/json" })
+  );
+  if (localStorage.get("ACCESS_TOKEN")) {
+    return new Promise((res) => {
+      axios
+        .post("/product/orders/payment/card/prepare/carts", formData, {
+          headers: {
+            "Content-Type": `multipart/form-data`,
+            Authorization: localStorage.getItem("ACCESS_TOKEN"),
+          },
+        })
+        .then((response) => {
+          res(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+          alert("카드결제 요청에 실패했습니다.");
+        });
+    });
+  } else {
+    return new Promise((res) => {
+      axios
+        .post("/product/guest/orders/payment/card/prepare/carts", formData, {
+          headers: {
+            "Content-Type": `multipart/form-data`,
+            Authorization: formData.destinationDto.receiverPhoneNumber,
+          },
+        })
+        .then((response) => {
+          res(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+          alert("카드결제 요청에 실패했습니다.");
+        });
+    });
+  }
 }
 
 export function checkPayApproval(orderId) {
-    return axios
-        .get('/product/orders/payment/card/confirm/'+orderId,
-            {
-                headers: {
-                    'Content-Type': `application/json`,
-                    'Authorization': localStorage.getItem('ACCESS_TOKEN')
-                }
-            }
-        );
+  return axios.get("/product/orders/payment/card/confirm/" + orderId, {
+    headers: {
+      "Content-Type": `application/json`,
+      Authorization: localStorage.getItem("ACCESS_TOKEN"),
+    },
+  });
 }
 
 export function getVBankInfos() {
-    return axios.get("/product/orders/payment/vbank")
+  return axios.get("/product/orders/payment/vbank");
 }
 
 export function payByVBank(dto, file) {
-    const formData = new FormData();
-    formData.append('imageFile', file);
-    formData.append(
-        'dto',
-        new Blob([JSON.stringify(dto)], { type: 'application/json' })
-    );
-    return new Promise(res => {
-        axios
-            .post('/product/orders/payment/vbank',
-                formData,
-                {
-                    headers: {
-                        'Content-Type': `multipart/form-data`,
-                        'Authorization': localStorage.getItem('ACCESS_TOKEN'),
-                    }
-                }
-            )
-            .then(response => {
-                res(response.data);
-            })
-            .catch(e => {
-                const eData = e.response.data
-                alert(`가상계좌 결제 요청이 실패하였습니다.\n에러코드: ${eData.errorCode}\n에러메시지:\n${eData.errorMessage}`);
-            })
+  const formData = new FormData();
+  formData.append("imageFile", file);
+  formData.append(
+    "dto",
+    new Blob([JSON.stringify(dto)], { type: "application/json" })
+  );
+  if (localStorage.get("ACCESS_TOKEN")) {
+    return new Promise((res) => {
+      axios
+        .post("/product/orders/payment/vbank", formData, {
+          headers: {
+            "Content-Type": `multipart/form-data`,
+            Authorization: localStorage.getItem("ACCESS_TOKEN"),
+          },
+        })
+        .then((response) => {
+          res(response.data);
+        })
+        .catch((e) => {
+          const eData = e.response.data;
+          alert(
+            `가상계좌 결제 요청이 실패하였습니다.\n에러코드: ${eData.errorCode}\n에러메시지:\n${eData.errorMessage}`
+          );
+        });
     });
+  } else {
+    return new Promise((res) => {
+      axios
+        .post("/product/guest/orders/payment/vbank", formData, {
+          headers: {
+            "Content-Type": `multipart/form-data`,
+            Authorization: formData.destinationDto.receiverPhoneNumber,
+          },
+        })
+        .then((response) => {
+          res(response.data);
+        })
+        .catch((e) => {
+          const eData = e.response.data;
+          alert(
+            `가상계좌 결제 요청이 실패하였습니다.\n에러코드: ${eData.errorCode}\n에러메시지:\n${eData.errorMessage}`
+          );
+        });
+    });
+  }
+}
+
+export function payByVBankCart(dto) {
+  const formData = new FormData();
+  formData.append(
+    "dto",
+    new Blob([JSON.stringify(dto)], { type: "application/json" })
+  );
+  if (localStorage.get("ACCESS_TOKEN")) {
+    return new Promise((res) => {
+      axios
+        .post("/product/orders/payment/vbank/carts", formData, {
+          headers: {
+            "Content-Type": `multipart/form-data`,
+            Authorization: localStorage.getItem("ACCESS_TOKEN"),
+          },
+        })
+        .then((response) => {
+          res(response.data);
+        })
+        .catch((e) => {
+          const eData = e.response.data;
+          alert(
+            `가상계좌 결제 요청이 실패하였습니다.\n에러코드: ${eData.errorCode}\n에러메시지:\n${eData.errorMessage}`
+          );
+        });
+    });
+  } else {
+    return new Promise((res) => {
+      axios
+        .post("/product/guest/orders/payment/vbank/carts", formData, {
+          headers: {
+            "Content-Type": `multipart/form-data`,
+            Authorization: formData.destinationDto.receiverPhoneNumber,
+          },
+        })
+        .then((response) => {
+          res(response.data);
+        })
+        .catch((e) => {
+          const eData = e.response.data;
+          alert(
+            `가상계좌 결제 요청이 실패하였습니다.\n에러코드: ${eData.errorCode}\n에러메시지:\n${eData.errorMessage}`
+          );
+        });
+    });
+  }
 }
