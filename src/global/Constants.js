d532061e3fa5a7249ca1d89513ab1af619bba1cc
tjswjd7166 <tjswjd7166@gmail.com>
2023-05-08 07:01:59 +0900

    global되돌림

--- src/global/Constants.js
+++ src/global/Constants.js
@@ -1,4 +1,4 @@
-/*import { SOCIAL_LOGIN_URL } from './url';
+import { SOCIAL_LOGIN_URL } from './url';
 import facebookLogo from '../image/icon/facebook-logo.png';
 
 const SOCIAL_LOGIN_PROVIDER = {
@@ -33,45 +33,3 @@ export { SOCIAL_LOGIN_PROVIDER, HTML_EDITOR_MODE };
 export const API_LOGIN_GOOGLE = `${SOCIAL_LOGIN_URL}/google`;
 export const API_LOGIN_FACEBOOK = `${SOCIAL_LOGIN_URL}/facebook`;
 export const API_LOGIN_NAVER = `${SOCIAL_LOGIN_URL}/naver`;
-*/
-
-import facebookLogo from '../image/icon/facebook-logo.png';
-
-export const SOCIAL_LOGIN_URL = {
-  NAVER: '/auth/naver',
-  KAKAO: '/auth/kakao',
-  GOOGLE: '/auth/google',
-  FACEBOOK: '/auth/facebook',
-};
-
-export const SOCIAL_LOGIN_PROVIDER = {
-  NAVER: {
-    img: 'https://vendor-cdn.imweb.me/images/naver_login2x.png',
-    url: SOCIAL_LOGIN_URL.NAVER,
-    css: 'naver',
-  },
-  KAKAO: {
-    img: 'https://vendor-cdn.imweb.me/images/kakao_icon.png',
-    url: SOCIAL_LOGIN_URL.KAKAO,
-    css: 'kakao',
-  },
-  GOOGLE: {
-    img: 'https://vendor-cdn.imweb.me/images/google_icon.png',
-    url: SOCIAL_LOGIN_URL.GOOGLE,
-    css: 'google',
-  },
-  FACEBOOK: {
-    img: facebookLogo,
-    url: SOCIAL_LOGIN_URL.FACEBOOK,
-    css: 'facebook',
-  },
-};
-
-export const HTML_EDITOR_MODE = {
-  ADD: 'ADD',
-  UPDATE: 'UPDATE',
-};
-
-export const API_LOGIN_GOOGLE = `${SOCIAL_LOGIN_URL.GOOGLE}/google`;
-export const API_LOGIN_FACEBOOK = `${SOCIAL_LOGIN_URL.FACEBOOK}/facebook`;
-export const API_LOGIN_NAVER = `${SOCIAL_LOGIN_URL.NAVER}/naver`;
