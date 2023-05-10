629d2bb98acfc2ffcbc46990a66b1d52a0b02172
2hanbyeol1 <2hanbyeol1@naver.com>
2023-05-10 12:54:02 +0900

    주문 페이지에서 옵션 및 이미지 미선택 시 alert + 스크롤 이동

--- src/component/Radio.js
+++ src/component/Radio.js
@@ -1,31 +1,30 @@
-import './Radio.css';
+import "./Radio.css";
 
 export default function Radio(props) {
   const radioButtons = document.querySelectorAll(
-    'input[type="radio"][name=' + props.name + ']'
+    'input[type="radio"][name=' + props.name + "]"
   );
 
   return (
     <div className="radio" style={props.style}>
       <input
-        id={'radio ' + props.text}
+        id={"radio " + props.text}
         type="radio"
         name={props.name}
         value={props.text}
-        required={props.required}
         checked={props.checked}
-        onChange={e => {
+        onChange={(e) => {
           props.onChange(e);
-          radioButtons.forEach(rb => {
+          radioButtons.forEach((rb) => {
             if (rb === e.target) {
-              if (!rb.checked) rb.toggleAttribute('checked');
+              if (!rb.checked) rb.toggleAttribute("checked");
             } else {
-              if (rb.checked) rb.toggleAttribute('checked');
+              if (rb.checked) rb.toggleAttribute("checked");
             }
           });
         }}
       />
-      <label htmlFor={'radio ' + props.text}>{props.text}</label>
+      <label htmlFor={"radio " + props.text}>{props.text}</label>
     </div>
   );
 }
