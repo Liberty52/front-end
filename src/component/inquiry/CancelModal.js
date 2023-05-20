a0cc4df37bb9db217fbbba2b21aad32e2f2f1315
한별 <78421872+2hanbyeol1@users.noreply.github.com>
2023-05-21 01:35:36 +0900

    [LIB-257] 회원 주문 취소 (#125)
    * 주문 취소 모달 창, axios 코드
    
    * 로그 삭제

---
+++ src/component/inquiry/CancelModal.js
@@ -0,0 +1,74 @@
+import "./CancelModal.css";
+import Modal from "../../component/Modal";
+import Button from "../../component/Button";
+import Input from "../../component/Input";
+import { cancelOrder } from "../../axios/shopping/Inquiry";
+
+import { useState } from "react";
+
+export default function CancelModal({ order, closeModal }) {
+  const [text, setText] = useState("");
+  const isAccount = order.paymentType == "가상 계좌";
+
+  return (
+    <Modal title="주문 취소" closeModal={closeModal}>
+      <form
+        className="cancel-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          const dto = {
+            orderId: order.orderId,
+            reason: e.target.reason.value,
+          };
+          if (isAccount) {
+            dto["refundBank"] = e.target.refundBank.value;
+            dto["refundHolder"] = e.target.refundHolder.value;
+            dto["refundAccount"] = e.target.refundAccount.value;
+            dto["refundPhoneNum"] = e.target.refundPhoneNum.value;
+          }
+          cancelOrder(dto);
+          closeModal();
+        }}
+      >
+        <div className="reason-title">취소 사유를 작성해주세요</div>
+        <textarea
+          className="reason-textarea"
+          name="reason"
+          value={text}
+          required
+          maxLength={1000}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        {isAccount && (
+          <>
+            <div className="refund-title">
+              환불받을 계좌 정보를 입력해주세요
+            </div>
+            <Input type="text" label="은행" name="refundBank" required />
+            <Input type="text" label="예금주명" name="refundHolder" required />
+            <Input
+              type="text"
+              label="계좌번호"
+              name="refundAccount"
+              required
+              pattern="[0-9]{1,19}"
+              maxLength={19}
+            />
+            <Input
+              type="text"
+              name="refundPhoneNum"
+              label="예금주 연락처"
+              required
+              pattern="01[0,1][0-9]{6,8}"
+              maxLength={11}
+              title="ex) 01012341234"
+            />
+          </>
+        )}
+        <Button text="주문 취소"></Button>
+      </form>
+    </Modal>
+  );
+}
