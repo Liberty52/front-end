4955387afee2abfda0a99e7b54383f07a766907e
JIN-YONG LEE <74762475+jinlee0@users.noreply.github.com>
2023-05-22 00:49:21 +0900

    텍스트로 이미지 생성 기능 추가 (#130)
    * 이미지 생성 기능 추가
    
    * 비로그인 유저 접근 제한 UI
    
    * AI 생성 탭 아이콘 변경
    
    * do not set opacity

---
+++ src/axios/shopping/AI.js
@@ -0,0 +1,15 @@
+import { ACCESS_TOKEN } from '../../constants/token'
+import axios from '../axios'
+
+export const postImageGeneration = async (prompt) => {
+  const response = await axios.post(
+    '/product/images/generations',
+    { prompt, n: 1 },
+    {
+      headers: {
+        Authorization: sessionStorage.getItem(ACCESS_TOKEN),
+      },
+    }
+  )
+  return response.data.urls
+}
