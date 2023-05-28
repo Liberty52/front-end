127f84b8248f4ce11df966b167c0cdff7658e95d
tjswjd7166 <tjswjd7166@gmail.com>
2023-05-29 05:53:37 +0900

    주문조회수정

--- src/global/Constants.js
+++ src/global/Constants.js
@@ -1,4 +1,4 @@
-/*import facebookLogo from '../image/icon/facebook-logo.png';
+import facebookLogo from '../image/icon/facebook-logo.png';
 import { SOCIAL_LOGIN_URL } from "./url";
 
 export const SOCIAL_LOGIN_PROVIDER = {
@@ -28,43 +28,6 @@ export const HTML_EDITOR_MODE = {
   ADD: 'ADD',
   UPDATE: 'UPDATE',
 };
-*/
-import facebookLogo from '../image/icon/facebook-logo.png';
-
-export const SOCIAL_LOGIN_URL = {
-  NAVER: '/auth/naver',
-  KAKAO: '/auth/kakao',
-  GOOGLE: '/auth/google',
-  FACEBOOK: '/auth/facebook',
-};
-
-export const SOCIAL_LOGIN_PROVIDER = {
-  NAVER: {
-    img: 'https://vendor-cdn.imweb.me/images/naver_login2x.png',
-    url: SOCIAL_LOGIN_URL.NAVER,
-    css: 'naver',
-  },
-  KAKAO: {
-    img: 'https://vendor-cdn.imweb.me/images/kakao_icon.png',
-    url: SOCIAL_LOGIN_URL.KAKAO,
-    css: 'kakao',
-  },
-  GOOGLE: {
-    img: 'https://vendor-cdn.imweb.me/images/google_icon.png',
-    url: SOCIAL_LOGIN_URL.GOOGLE,
-    css: 'google',
-  },
-  FACEBOOK: {
-    img: facebookLogo,
-    url: SOCIAL_LOGIN_URL.FACEBOOK,
-    css: 'facebook',
-  },
-};
-/////////////////////////
-export const HTML_EDITOR_MODE = {
-  ADD: 'ADD',
-  UPDATE: 'UPDATE',
-};
 
 export const API_LOGIN_GOOGLE = `${SOCIAL_LOGIN_URL.GOOGLE}/google`;
 export const API_LOGIN_FACEBOOK = `${SOCIAL_LOGIN_URL.FACEBOOK}/facebook`;
@@ -73,4 +36,3 @@ export const API_LOGIN_NAVER = `${SOCIAL_LOGIN_URL.NAVER}/naver`;
 export const MOUNTING_METHOD = ["이젤 거치형", "벽걸이형"]
 export const BASIC_MATERIAL = ["1mm 두께 승화전사 인쇄용 알루미늄시트"]
 export const ADDITIONAL_MATERIAL = ["유광실버", "무광실버", "유광백색", "무광백색"]
-
