30c0aabe9b8dfea04b284affd7d64789c376b8fc
tjswjd7166 <tjswjd7166@gmail.com>
2023-05-30 12:11:26 +0900

    버튼위치변경

--- src/global/Constants.js
+++ src/global/Constants.js
@@ -1,52 +1,5 @@
-<<<<<<< Updated upstream
-import { SOCIAL_LOGIN_URL } from './url';
-=======
-/*import facebookLogo from '../image/icon/facebook-logo.png';
-import { SOCIAL_LOGIN_URL } from "./url";
->>>>>>> Stashed changes
-
-const SOCIAL_LOGIN_PROVIDER = {
-  NAVER: {
-    text: "네이버 로그인",
-    url: SOCIAL_LOGIN_URL.NAVER,
-    css: "naver",
-  },
-  KAKAO: {
-    text: "카카오 로그인",
-    url: SOCIAL_LOGIN_URL.KAKAO,
-    css: "kakao",
-  },
-  GOOGLE: {
-    text: "구글 로그인",
-    url: SOCIAL_LOGIN_URL.GOOGLE,
-    css: "google",
-  },
-  FACEBOOK: {
-    text: "페이스북 로그인",
-    url: SOCIAL_LOGIN_URL.FACEBOOK,
-    css: "facebook",
-  },
-};
-
-<<<<<<< Updated upstream
-const HTML_EDITOR_MODE = {
-  ADD : "ADD",
-  UPDATE : "UPDATE"
-}
-=======
-export const HTML_EDITOR_MODE = {
-  ADD: 'ADD',
-  UPDATE: 'UPDATE',
-};
-*/
 import facebookLogo from '../image/icon/facebook-logo.png';
-
-export const SOCIAL_LOGIN_URL = {
-  NAVER: '/auth/naver',
-  KAKAO: '/auth/kakao',
-  GOOGLE: '/auth/google',
-  FACEBOOK: '/auth/facebook',
-};
+import { SOCIAL_LOGIN_URL } from "./url";
 
 export const SOCIAL_LOGIN_PROVIDER = {
   NAVER: {
@@ -70,14 +23,17 @@ export const SOCIAL_LOGIN_PROVIDER = {
     css: 'facebook',
   },
 };
-/////////////////////////
+
+
 export const HTML_EDITOR_MODE = {
   ADD: 'ADD',
   UPDATE: 'UPDATE',
 };
->>>>>>> Stashed changes
 
-export { SOCIAL_LOGIN_PROVIDER, HTML_EDITOR_MODE };
-export const API_LOGIN_GOOGLE = `${SOCIAL_LOGIN_URL}/google`;
-export const API_LOGIN_FACEBOOK = `${SOCIAL_LOGIN_URL}/facebook`;
-export const API_LOGIN_NAVER = `${SOCIAL_LOGIN_URL}/naver`;
+export const API_LOGIN_GOOGLE = `${SOCIAL_LOGIN_URL.GOOGLE}/google`;
+export const API_LOGIN_FACEBOOK = `${SOCIAL_LOGIN_URL.FACEBOOK}/facebook`;
+export const API_LOGIN_NAVER = `${SOCIAL_LOGIN_URL.NAVER}/naver`;
+
+export const MOUNTING_METHOD = ["이젤 거치형", "벽걸이형"]
+export const BASIC_MATERIAL = ["1mm 두께 승화전사 인쇄용 알루미늄시트"]
+export const ADDITIONAL_MATERIAL = ["유광실버", "무광실버", "유광백색", "무광백색"]
