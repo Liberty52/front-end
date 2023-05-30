f12fa42ba8db3ee3c1a5bf5ee2a325e672810f6a
tjswjd7166 <tjswjd7166@gmail.com>
2023-05-30 12:07:33 +0900

    버튼위치수정

--- src/global/Constants.js
+++ src/global/Constants.js
@@ -1,4 +1,9 @@
+<<<<<<< Updated upstream
 import { SOCIAL_LOGIN_URL } from './url';
+=======
+/*import facebookLogo from '../image/icon/facebook-logo.png';
+import { SOCIAL_LOGIN_URL } from "./url";
+>>>>>>> Stashed changes
 
 const SOCIAL_LOGIN_PROVIDER = {
   NAVER: {
@@ -23,10 +28,54 @@ const SOCIAL_LOGIN_PROVIDER = {
   },
 };
 
+<<<<<<< Updated upstream
 const HTML_EDITOR_MODE = {
   ADD : "ADD",
   UPDATE : "UPDATE"
 }
+=======
+export const HTML_EDITOR_MODE = {
+  ADD: 'ADD',
+  UPDATE: 'UPDATE',
+};
+*/
+import facebookLogo from '../image/icon/facebook-logo.png';
+
+export const SOCIAL_LOGIN_URL = {
+  NAVER: '/auth/naver',
+  KAKAO: '/auth/kakao',
+  GOOGLE: '/auth/google',
+  FACEBOOK: '/auth/facebook',
+};
+
+export const SOCIAL_LOGIN_PROVIDER = {
+  NAVER: {
+    img: 'https://vendor-cdn.imweb.me/images/naver_login2x.png',
+    url: SOCIAL_LOGIN_URL.NAVER,
+    css: 'naver',
+  },
+  KAKAO: {
+    img: 'https://vendor-cdn.imweb.me/images/kakao_icon.png',
+    url: SOCIAL_LOGIN_URL.KAKAO,
+    css: 'kakao',
+  },
+  GOOGLE: {
+    img: 'https://vendor-cdn.imweb.me/images/google_icon.png',
+    url: SOCIAL_LOGIN_URL.GOOGLE,
+    css: 'google',
+  },
+  FACEBOOK: {
+    img: facebookLogo,
+    url: SOCIAL_LOGIN_URL.FACEBOOK,
+    css: 'facebook',
+  },
+};
+/////////////////////////
+export const HTML_EDITOR_MODE = {
+  ADD: 'ADD',
+  UPDATE: 'UPDATE',
+};
+>>>>>>> Stashed changes
 
 export { SOCIAL_LOGIN_PROVIDER, HTML_EDITOR_MODE };
 export const API_LOGIN_GOOGLE = `${SOCIAL_LOGIN_URL}/google`;
---
+++ src/screen/inquiry/Inquiry.js
@@ -0,0 +1,208 @@
+import "./Inquiry.css";
+import React, { useState, useEffect } from "react";
+import { fetchOrders } from "../../axios/inquiry/Inquiry";
+import Header from "../../component/common/Header";
+import Footer from "../../component/common/Footer";
+import ReviewModal from "../../component/order/review/ReviewModal";
+import CancelModal from "../../component/inquiry/CancelModal";
+import { useNavigate } from "react-router-dom";
+import Button from "../../component/common/Button";
+import { ACCESS_TOKEN } from "../../constants/token";
+
+export async function getAccessToken() {
+  return sessionStorage.getItem(ACCESS_TOKEN);
+}
+
+function OrderList() {
+  const navigate = useNavigate();
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    const fetchOrdersData = async () => {
+      try {
+        const accessToken = await getAccessToken();
+        const response = await fetchOrders(accessToken);
+        setOrders(response.data);
+      } catch (error) {
+
+      }
+    };
+
+    fetchOrdersData();
+  }, []);
+
+  function goToDetail(orderId) {
+    navigate(`/detail/${orderId}`);
+  }
+
+  const [reviewModal, showReviewModal] = useState(false);
+  const [cancelModal, showCancelModal] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState({});
+
+  return (
+    <>
+     <>
+        {reviewModal && (
+          <ReviewModal
+            orderId={selectedOrder.orderId}
+            closeModal={() => showReviewModal(false)}
+          />
+        )}
+        {cancelModal && (
+          <CancelModal
+            order={selectedOrder}
+            closeModal={() => showCancelModal(false)}
+          />
+        )}
+
+        <div className="content">
+          <div className="TCheck">주문조회</div>
+          <div className="sectionOrder">
+            {orders.map((order) => (
+              <div
+                key={order.orderId}
+                className="order-item"
+                onClick={() => goToDetail(order.orderId)}
+              >
+                <OrderImg
+                  orderId={order.orderId}
+                  orderNum={order.orderNum}
+                  productRepresentUrl={order.productRepresentUrl}
+                  goToDetail={goToDetail}
+                  showCancelModal={showCancelModal}
+                  setSelectedOrder={setSelectedOrder}
+                  orderStatus={order.orderStatus}
+                />
+                <OrderInfo
+                  order={order}
+                  showReviewModal={showReviewModal}
+                  showCancelModal={showCancelModal}
+                  setSelectedOrder={setSelectedOrder}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        </>
+    </>
+  );
+  function OrderImg({ orderId, orderNum, productRepresentUrl, goToDetail, showCancelModal, setSelectedOrder, orderStatus }) {
+    return (
+      <div className="order-img-wrapper">
+        <div className="order-left">
+          <p>{orderNum}</p>
+          <img
+            src={productRepresentUrl}
+            alt="representative"
+            className="productRepresentUrl"
+          />
+          <CancelOrderButton
+            order={{orderId, orderStatus}}
+            showCancelModal={showCancelModal}
+            setSelectedOrder={setSelectedOrder}
+          />
+        </div>
+      </div>
+    );
+  }
+
+
+  function OrderInfo({
+    order,
+    showReviewModal,
+    showCancelModal,
+    setSelectedOrder,
+  }) {
+    return (
+      <div className="order-info-wrapper">
+          <div className="order-right-top">
+            <ul>
+              {order.products.map((product) => (
+                <li className="product-item" key={product.name}>
+                  <p>{product.name}</p>
+                  <p>{product.quantity} 개</p>
+                  <p>₩{product.price.toLocaleString()}</p>
+                  {(order.orderStatus === "ORDERED" || order.orderStatus === "WAITING_DEPOSIT") && (
+                    <div className="buttons">
+                      <Button
+                        type="button"
+                        className="review"
+                        text="리뷰 쓰기"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          showReviewModal(true);
+                          setSelectedOrder(product);
+                        }}
+                      />
+                    </div>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="order-right-bottom">
+
+            <div className="PayAdd-wrapper">
+              <div className="payment-info">결제 : </div>
+              <div className="inquiry-card">
+                {order.paymentType} {order.paymentInfo.cardName}{" "}
+                {order.paymentInfo.cardNumber}
+              </div>
+              <div className="address2">배송지 :</div>
+              <div className="inquiry-address2">{order.address}</div>
+            </div>
+
+            <div className="personal-info">
+              <div className="info-phone">연락처 정보: </div>
+              <div className="email">{order.receiverEmail}</div>
+              <div className="phonenumber">{order.receiverPhoneNumber}</div>
+              <div className="info-customer">주문자명 : </div>
+              <div className="Ordername">{order.receiverName}</div>
+            </div>
+
+            <div className="order-status-wrapper">
+              <div className="order-status">
+
+                주문 상태 : </div>
+                <div className="order-status-value">
+                  {order.orderStatus}
+                </div>
+              <div className="date">주문 날짜 : </div>
+              <div className="order-date">{order.orderDate}</div>
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function CancelOrderButton({ order, showCancelModal, setSelectedOrder }) {
+  return (
+    (order.orderStatus === "ORDERED" || order.orderStatus === "WAITING_DEPOSIT") && (
+      <div className="cancel-button">
+        <button
+          type="button"
+          className="cancel"
+          onClick={(e) => {
+            e.stopPropagation();
+            showCancelModal(true);
+            setSelectedOrder(order);
+          }}
+        >
+          주문 취소
+        </button>
+      </div>
+    )
+  );
+}
+
+
+export default function inquiry() {
+  return (
+    <div className="inquiry">
+      <Header />
+      <OrderList />
+      <Footer />
+    </div>
+  );
+}
+
