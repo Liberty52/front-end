ffff3a1841680d0139c0d77b1acebaa1b7d50298
한별 <78421872+2hanbyeol1@users.noreply.github.com>
2023-06-05 15:38:28 +0900

    [ISSUE-164] 회원탈퇴 후 메인페이지로 이동 (#165)
    * 환불 계좌 입력창
    
    * 로그인 dto 수정: checked 추가
    
    * 비밀번호 찾기에서 이메일 input 오류
    
    * 로그인 일부 Refactoring
    
    * 환영합니다 alert 삭제
    
    * 회원 탈퇴 후 메인페이지로 이동 및 리팩토링
    
    * 변수명 checked -> isAutoLogin
    
    ---------
    
    Co-authored-by: 김규현 <76154390+kgh2120@users.noreply.github.com>

--- src/axios/myInfo/MyInfo.js
+++ src/axios/myInfo/MyInfo.js
@@ -1,62 +1,55 @@
-import axios from '../axios';
+import axios from "../axios";
 import { ACCESS_TOKEN } from "../../constants/token";
 
 export function delMyInfo() {
-  if (window.confirm('정말로 탈퇴하시겠습니까?')) {
-    axios
-      .delete(`/auth/member`, {
-        headers: {
-          Authorization:  sessionStorage.getItem(ACCESS_TOKEN),
-        },
-      })
-      .then(() => {
-        localStorage.clear();
-        sessionStorage.clear();
-        alert('탈퇴가 성공적으로 이루어졌습니다.');
-        // navigate("/");
-      })
-      .catch(err => alert(err.response.data.message));
+  if (window.confirm("정말로 탈퇴하시겠습니까?")) {
+    return axios.delete(`/auth/member`, {
+      headers: {
+        Authorization: sessionStorage.getItem(ACCESS_TOKEN),
+      },
+    });
   }
+  return null;
 }
 
 export function putMyInfo(dto, file) {
   const formData = new FormData();
-  formData.append('file', file);
+  formData.append("file", file);
   formData.append(
-    'dto',
-    new Blob([JSON.stringify(dto)], { type: 'application/json' })
+    "dto",
+    new Blob([JSON.stringify(dto)], { type: "application/json" })
   );
-  return new Promise(res => {
+  return new Promise((res) => {
     axios
-      .put('/auth/my', formData, {
+      .put("/auth/my", formData, {
         headers: {
-          Authorization:  sessionStorage.getItem(ACCESS_TOKEN),
-          'Contest-Type': 'multipart/form-data',
+          Authorization: sessionStorage.getItem(ACCESS_TOKEN),
+          "Contest-Type": "multipart/form-data",
         },
       })
       .then(() => {
-        alert('회원정보 수정이 완료되었습니다.');
+        alert("회원정보 수정이 완료되었습니다.");
         res(getMyInfo());
       })
-      .catch(e => {
+      .catch((e) => {
         if (e.response) {
           if (e.response.status === 400)
-            alert('기존 비밀번호가 올바르지 않습니다.');
-          else if (e.response.status === 401) alert('아이디 오류');
+            alert("기존 비밀번호가 올바르지 않습니다.");
+          else if (e.response.status === 401) alert("아이디 오류");
         }
       });
   });
 }
 
 export function getMyInfo() {
-  return new Promise(res => {
+  return new Promise((res) => {
     axios
-      .get('/auth/my', {
+      .get("/auth/my", {
         headers: {
           Authorization: sessionStorage.getItem(ACCESS_TOKEN),
         },
       })
-      .then(response => {
+      .then((response) => {
         const info = {
           profileUrl: response.data.profileUrl,
           email: response.data.email,
@@ -65,8 +58,8 @@ export function getMyInfo() {
         };
         res(info);
       })
-      .catch(e => {
-        alert('정보를 가져오는데 실패했습니다.');
+      .catch((e) => {
+        alert("정보를 가져오는데 실패했습니다.");
       });
   });
 }
