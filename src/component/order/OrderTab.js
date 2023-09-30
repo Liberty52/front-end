a6659233c5580bf530a57bb1f99730cb74e5ad78
한별 <78421872+2hanbyeol1@users.noreply.github.com>
2023-09-30 21:11:24 +0900

    [FEAT] 상품 소개, 리뷰 탭으로 변경 (#204)
    * [FEAT] 상품 소개, 리뷰 탭으로 변경
    
    * [ui] 주문 탭 반응형

---
+++ src/component/order/OrderTab.js
@@ -0,0 +1,103 @@
+import { Box, Tabs, Tab } from "@mui/material";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import Review from "./review/Review";
+import Introduction from "./Introduction";
+import styled from "styled-components";
+
+const OrderTabBox = styled(Box)`
+  margin: 0 200px;
+  @media screen and (max-width: 768px) {
+    margin: 0;
+  }
+`;
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`product-tabpanel-${index}`}
+      aria-labelledby={`product-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `product-tab-${index}`,
+    "aria-controls": `product-tabpanel-${index}`,
+  };
+}
+
+export default function OrderTab({ introductionImageUrl }) {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const tabConfigs = [
+    {
+      value: "intro",
+      label: "상품 소개",
+      content: <Introduction introductionImageUrl={introductionImageUrl} />,
+    },
+    {
+      value: "review",
+      label: "리뷰",
+      content: <Review />,
+    },
+  ];
+
+  return (
+    <OrderTabBox>
+      <Box
+        sx={{
+          borderBottom: 1,
+          borderColor: "divider",
+        }}
+      >
+        <Tabs
+          sx={{ mt: 7 }}
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+          centered
+        >
+          {tabConfigs.map((t, i, arr) => {
+            return (
+              <Tab
+                key={i}
+                sx={{
+                  fontSize: 18,
+                  fontWeight: "bold",
+                }}
+                label={t.label}
+                {...a11yProps(i)}
+              />
+            );
+          })}
+        </Tabs>
+      </Box>
+      {tabConfigs.map((t, i, arr) => {
+        return (
+          <TabPanel key={i} value={value} index={i}>
+            {t.content}
+          </TabPanel>
+        );
+      })}
+    </OrderTabBox>
+  );
+}
