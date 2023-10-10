8adc58351ab07c390400e5013a4be848a1451121
heeeione <heewon410@naver.com>
2023-10-10 14:11:45 +0900

    [Style] 라이센스 헤더

--- src/component/order/editor/panels/panelItems/License.js
+++ src/component/order/editor/panels/panelItems/License.js
@@ -1,3 +1,31 @@
-const License = () => {};
+import { Block } from "baseui/block";
+import useSidebarOpen from "../../../../../hooks/useSidebarOpen";
+import AngleDoubleLeft from "../../icons/AngleDoubleLeft";
+
+const License = () => {
+  const { setIsSidebarOpen } = useSidebarOpen();
+  return (
+    <Block $style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+      <Block
+        $style={{
+          display: "flex",
+          alignItems: "center",
+          fontWeight: 500,
+          justifyContent: "space-between",
+          padding: "1.5rem",
+        }}
+      >
+        <Block>라이센스</Block>
+
+        <Block
+          onClick={() => setIsSidebarOpen(false)}
+          $style={{ cursor: "pointer", display: "flex" }}
+        >
+          <AngleDoubleLeft size={18} />
+        </Block>
+      </Block>
+    </Block>
+  );
+};
 
 export default License;
