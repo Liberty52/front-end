4253c9a0f04ee0b65ab499c5689f7a450e73c072
KimHyeongKeun <107840065+KimHyeongKeun@users.noreply.github.com>
2023-10-25 18:12:47 +0900

    상품 출력 페이지 (#246)

--- src/axios/order/Order.js
+++ src/axios/order/Order.js
@@ -1,6 +1,7 @@
+import axios from "../axios";
 import request from "../axios";
 import { ACCESS_TOKEN } from "../../constants/token";
-import { PRODUCT_INFO } from "../../constants/api";
+import { PRODUCT_INFO, PRODUCT_LIST } from "../../constants/api";
 
 export const getProductInfo = async (id) => {
   return request.get(PRODUCT_INFO(id), {
@@ -9,3 +10,24 @@ export const getProductInfo = async (id) => {
     },
   });
 };
+
+//https://liberty52.com:444/products?page=0&size=10
+export function getProductList(page, size) {
+  return new Promise((res) => {
+    axios
+      .get(
+        `/product/products?page=${page}&size=${size}`,
+        {
+          headers: {
+            Authorization: sessionStorage.getItem(ACCESS_TOKEN),
+          },
+        }
+      )
+      .then((response) => {
+        res(response.data);
+      })
+      .catch((e) =>{
+        console.error(e);
+      } );
+  });
+}
--- src/screen/main/Main.js
+++ src/screen/main/Main.js
@@ -20,7 +20,7 @@ function Section() {
         <div className="text1">Liberty 52_Frame</div>
         <div className="text2">One & Only Speaker</div>
         <div className="text3">내가 만드는 세상 유일</div>
-        <a className="order-button" href="/order">
+        <a className="choose-button" href="/choose">
           구매하기
         </a>
       </div>
@@ -41,7 +41,7 @@ function Section2() {
         <p style={{ fontSize: "20px" }}>
           1mm 두께 승화전사 인쇄용 알루미늄시트
         </p>
-        <a className="order-button" href="/order">
+        <a className="choose-button" href="/choose">
           구매하기
         </a>
       </div>
@@ -61,7 +61,7 @@ function Section3() {
       <div className="text-group">
         <h1>진동 블루투스 스피커</h1>
         <p style={{ fontSize: "20px" }}>고성능 블루투스 스피커 탑재</p>
-        <a className="order-button" href="/order">
+        <a className="choose-button" href="/choose">
           구매하기
         </a>
       </div>
@@ -76,7 +76,7 @@ function Section4() {
       <div className="text-group">
         <h1>세계 유일</h1>
         <p style={{ fontSize: "20px" }}>나만의 액자형 스피커</p>
-        <a className="order-button" href="/order">
+        <a className="choose-button" href="/choose">
           구매하기
         </a>
       </div>
