ac3955278b2579e201fadcd7223cc0dafe299d34
heeeione <heewon410@naver.com>
2023-10-31 13:33:55 +0900

    [Feat] choose 페이지에서 order 페이지로 id 넘기기

--- src/screen/order/Choose.js
+++ src/screen/order/Choose.js
@@ -1,14 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
-import "./Choose.css";
-import React, { useState, useEffect } from "react";
-import Footer from "../../component/common/Footer";
-import { Rating, Card } from "@mui/material";
-import { Row } from "react-bootstrap";
-import { getProductList } from "../../axios/order/Order";
-import { useNavigate } from "react-router-dom";
-import Header from "../../component/common/Header";
-import ReviewModal from "../../component/order/review/ReviewModal";
+import './Choose.css';
+import React, { useState, useEffect } from 'react';
+import Footer from '../../component/common/Footer';
+import { Rating, Card } from '@mui/material';
+import { Row } from 'react-bootstrap';
+import { getProductList } from '../../axios/order/Order';
+import { useNavigate } from 'react-router-dom';
+import Header from '../../component/common/Header';
+import ReviewModal from '../../component/order/review/ReviewModal';
 
 export default function Choose() {
   function ChooseContents(id) {
@@ -16,11 +16,7 @@ export default function Choose() {
     for (var i = 0; i < productContents.length; i++) {
       list.push(<ProductContent key={i} productInfo={productContents[i]} />);
     }
-    return (
-      <div>
-        {list.length > 0 ? list : <span>상품이 없습니다.</span>}
-      </div>
-    );
+    return <div>{list.length > 0 ? list : <span>상품이 없습니다.</span>}</div>;
   }
 
   function getProductFromServer() {
@@ -50,92 +46,96 @@ export default function Choose() {
   useEffect(() => {
     getProductFromServer();
   }, []);
- 
+
   function ProductContent(props) {
     const [modal, showModal] = useState(false);
     const productInfo = props.productInfo;
+    const navigate = useNavigate();
+    const cardClicked = () => {
+      navigate(`/order`, {
+        state: {
+          productId: productInfo.id,
+        },
+      });
+    };
 
     return (
       <>
-      <div className="choose">
-      {modal ? (
-          <ReviewModal
-            closeModal={() => showModal(false)}
-            productInfo={productInfo}
-            onSuccess={() => {
-              getProductFromServer();
-            }}
-            
-          />
-        ) : (
-          <></>
-        )}
-     </div >
-      <div >
-            <div className="choose-mainContainer">
-               
-                <Card style={{ width: '20rem', height: '30rem' }}  onClick={cardClicked} >
-                <div className="product" >
-                    <div className="product">
-                            <div className="choose-product" key={productInfo.id}>
-                              
-                                <span className="product-image img">{
-                                   productInfo.id === "LIB-001"
-                                   ? <img src="https://liberty52.s3.ap-northeast-2.amazonaws.com/product/static/liberty52-frame.png"></img>
-                                   :<img  src={productInfo.pictureUrl}></img>
-                                }
-                                </span>
-                                <div className='product-contents'>
-                                <div className='choose-isnotsale'>  {
-                                        productInfo.state === "ON_SALE"
-                                        ? <div onClick={cardClicked}></div>
-                                        : (productInfo.state === "NOT_SALE"
-                                          ?<p className="choose-notsale">품절</p>
-                                          :<p>미판매</p>
-                                        )
-                                }</div>
-                                </div>
-                                <div className='product-contents' style={{}}>
-                                  <Row>
-                                    <span className='choose-productName'> {productInfo.name}</span>
-                                    <span className='choose-iscustomtext'>
-                                      {
-                                        productInfo.iscustom === "true"
-                                        ? "custom"
-                                        :<p>premium license</p>
-                                      }
-                                      </span>
-                                    <span className='choose-productPrice'> {productInfo.price}원</span>
-                                    <span>
-                                        <div className='choose-producttext'>  {
-                                            productInfo.state === "ON_SALE"
-                                            ? <></>
-                                            : (productInfo.state === "NOT_SALE"
-                                              ?<p>품절</p>
-                                              :<p>미판매</p>
-                                            )
-                                        }</div>
-                                    </span>
-                                    <div  className="choose-contents">
-                                    <Rating readOnly className="choose-contents" defaultValue={productInfo.meanRate} precision={0.5} />
-                                    </div>
-                                  </Row>
-                                </div>
-                            </div>
+        <div className='choose'>
+          {modal ? (
+            <ReviewModal
+              closeModal={() => showModal(false)}
+              productInfo={productInfo}
+              onSuccess={() => {
+                getProductFromServer();
+              }}
+            />
+          ) : (
+            <></>
+          )}
+        </div>
+        <div>
+          <div className='choose-mainContainer'>
+            <Card style={{ width: '20rem', height: '30rem' }} onClick={cardClicked}>
+              <div className='product'>
+                <div className='product'>
+                  <div className='choose-product' key={productInfo.id}>
+                    <span className='product-image img'>
+                      {productInfo.id === 'LIB-001' ? (
+                        <img src='https://liberty52.s3.ap-northeast-2.amazonaws.com/product/static/liberty52-frame.png'></img>
+                      ) : (
+                        <img src={productInfo.pictureUrl}></img>
+                      )}
+                    </span>
+                    <div className='product-contents'>
+                      <div className='choose-isnotsale'>
+                        {' '}
+                        {productInfo.state === 'ON_SALE' ? (
+                          <div onClick={cardClicked}></div>
+                        ) : productInfo.state === 'NOT_SALE' ? (
+                          <p className='choose-notsale'>품절</p>
+                        ) : (
+                          <p>미판매</p>
+                        )}
+                      </div>
                     </div>
+                    <div className='product-contents' style={{}}>
+                      <Row>
+                        <span className='choose-productName'> {productInfo.name}</span>
+                        <span className='choose-iscustomtext'>
+                          {productInfo.iscustom === 'true' ? 'custom' : <p>premium license</p>}
+                        </span>
+                        <span className='choose-productPrice'> {productInfo.price}원</span>
+                        <span>
+                          <div className='choose-producttext'>
+                            {' '}
+                            {productInfo.state === 'ON_SALE' ? (
+                              <></>
+                            ) : productInfo.state === 'NOT_SALE' ? (
+                              <p>품절</p>
+                            ) : (
+                              <p>미판매</p>
+                            )}
+                          </div>
+                        </span>
+                        <div className='choose-contents'>
+                          <Rating
+                            readOnly
+                            className='choose-contents'
+                            defaultValue={productInfo.meanRate}
+                            precision={0.5}
+                          />
+                        </div>
+                      </Row>
+                    </div>
+                  </div>
                 </div>
-                </Card>
-            </div>
+              </div>
+            </Card>
+          </div>
         </div>
       </>
-      
     );
-  }
-
-  const navigate = useNavigate();
-  const cardClicked = () => {
-    navigate(`/order`);
-     
   }
 
   function Pages(props) {
@@ -146,13 +146,13 @@ export default function Choose() {
         list.push(
           <span
             key={i}
-            className="active"
+            className='active'
             onClick={() => {
               setPageNum(i - 1);
             }}
           >
             {i}
-          </span>
+          </span>,
         );
       else
         list.push(
@@ -163,10 +163,10 @@ export default function Choose() {
             }}
           >
             {i}
-          </span>
+          </span>,
         );
     }
-    return <div className="pages">{list}</div>;
+    return <div className='pages'>{list}</div>;
   }
   const [productContents, setProductContents] = useState([]);
   const [pages, setPages] = useState({
@@ -177,26 +177,21 @@ export default function Choose() {
   const [pageNum, setPageNum] = useState(0);
 
   return (
-   <>
-      <Header/>
-      <div className="choose">
-
-        <div className="choose-container">
-          <h1 className="choose-product">상품</h1>
+    <>
+      <Header />
+      <div className='choose'>
+        <div className='choose-container'>
+          <h1 className='choose-product'>상품</h1>
           <Row>
-            <ChooseContents className="choose-product" productContents={productContents} />
+            <ChooseContents className='choose-product' productContents={productContents} />
           </Row>
         </div>
-
       </div>
 
-      <div className="review">
-      <Pages className="choose-page" pages={pages} />
-
+      <div className='review'>
+        <Pages className='choose-page' pages={pages} />
       </div>
       <Footer />
-
-    
-  </>
+    </>
   );
 }
