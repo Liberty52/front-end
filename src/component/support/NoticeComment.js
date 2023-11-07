715b5b41e083f1d43957c61674dc8a0c99fd48f2
한별 <78421872+2hanbyeol1@users.noreply.github.com>
2023-11-07 22:56:33 +0900

    [ISSUE-282] 공지 댓글 추가 작업 (#283)
    * [FEAT] 댓글 허용되지 않은 경우 컴포넌트 숨김
    
    * [API] 로그인 회원인 경우 댓글 조회 시 Authorization 추가

--- src/component/support/NoticeComment.js
+++ src/component/support/NoticeComment.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import {
   CommentListContainer,
   CommentContainer,
@@ -11,9 +11,9 @@ import {
   Content,
   Date,
   Page,
-} from "./style/Notice";
-import { postComment, retrieveComments } from "../../axios/support/Notice";
-import { ACCESS_TOKEN } from "../../constants/token";
+} from './style/Notice';
+import { postComment, retrieveComments } from '../../axios/support/Notice';
+import { ACCESS_TOKEN } from '../../constants/token';
 
 export default function NoticeComment({ noticeId }) {
   const [comments, setComments] = useState([]);
@@ -29,7 +29,7 @@ export default function NoticeComment({ noticeId }) {
   }, [pages.currentPage]);
 
   const getComment = async () => {
-    const res = await retrieveComments(noticeId, pages.currentPage);
+    const res = await retrieveComments(noticeId, pages.currentPage, logined);
     setComments(res.data.content);
     setPages({
       startPage: 1,
@@ -42,16 +42,16 @@ export default function NoticeComment({ noticeId }) {
     e.preventDefault();
 
     const content = e.target.content.value;
-    if (content === "") {
-      alert("댓글 내용을 입력해주세요");
+    if (content === '') {
+      alert('댓글 내용을 입력해주세요');
       return;
     }
     const res = await postComment(noticeId, content);
     if (res.status === 200) {
       getComment();
-      e.target.content.value = "";
+      e.target.content.value = '';
     } else if (res.status === 401) {
-      alert("로그인이 필요한 기능입니다");
+      alert('로그인이 필요한 기능입니다');
     }
   };
 
@@ -72,14 +72,14 @@ export default function NoticeComment({ noticeId }) {
     for (let i = 1; i <= pages.lastPage; i++) {
       pageComponents.push(
         <Page
-          style={{ fontWeight: i === pages.currentPage && "bold" }}
+          style={{ fontWeight: i === pages.currentPage && 'bold' }}
           key={i}
           onClick={() => {
             if (i !== pages.currentPage) setPages({ ...pages, currentPage: i });
           }}
         >
           {i}
-        </Page>
+        </Page>,
       );
     }
     return <PageContainer>{pageComponents}</PageContainer>;
@@ -89,9 +89,7 @@ export default function NoticeComment({ noticeId }) {
     <>
       <CommentListContainer>
         {comments.length === 0 ? (
-          <div style={{ textAlign: "center", marginBottom: "10px" }}>
-            작성된 댓글이 없습니다
-          </div>
+          <div style={{ textAlign: 'center', marginBottom: '10px' }}>작성된 댓글이 없습니다</div>
         ) : (
           comments.map((comment) => {
             return <CommentItem key={comment.commentId} comment={comment} />;
@@ -101,12 +99,12 @@ export default function NoticeComment({ noticeId }) {
       <Pages />
       <InputContainer onSubmit={submitComment}>
         <CommentInput
-          name="content"
-          variant="outlined"
-          placeholder={logined ? "" : "댓글 작성을 위해 로그인이 필요합니다"}
+          name='content'
+          variant='outlined'
+          placeholder={logined ? '' : '댓글 작성을 위해 로그인이 필요합니다'}
           disabled={!logined}
         />
-        <CommentButton type="submit" disabled={!logined}>
+        <CommentButton type='submit' disabled={!logined}>
           작성
         </CommentButton>
       </InputContainer>
@@ -115,10 +113,10 @@ export default function NoticeComment({ noticeId }) {
 }
 
 function formatDate(date) {
-  var dateParts = date.split("T");
+  var dateParts = date.split('T');
   var datePart = dateParts[0];
-  var timePart = dateParts[1].split(".")[0].slice(0, 5);
-  var formattedDate = datePart + " " + timePart;
+  var timePart = dateParts[1].split('.')[0].slice(0, 5);
+  var formattedDate = datePart + ' ' + timePart;
 
   return formattedDate;
 }
