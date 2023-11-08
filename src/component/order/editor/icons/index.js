1204ce77c1a2ef89a43047e3dd07d91c9fbf07ac
채희원 <68272931+heeeione@users.noreply.github.com>
2023-11-08 17:42:06 +0900

    [LIB-273] 이미지 에디터 텍스트 추가 (#274)
    * add: Text icon
    
    * init: Text
    
    * test
    
    * add: text
    
    * [Feat] 이미지 에디터 텍스트 추가

--- src/component/order/editor/icons/index.js
+++ src/component/order/editor/icons/index.js
@@ -4,6 +4,7 @@ import SwapHorizontal from './SwapHorizontal';
 import Layers from './Layers';
 import AI from './AI';
 import License from './License';
+import Text from './Text';
 class Icons {
   static Images = Images;
   static Uploads = Uploads;
@@ -11,6 +12,7 @@ class Icons {
   static Layers = Layers;
   static AI = AI;
   static License = License;
+  static Text = Text;
 }
 
 export default Icons;
