3019ec80e7f9b1657d06d95651e9fc60b3132268
2hanbyeol1 <2hanbyeol1@naver.com>
2023-11-09 22:57:47 +0900

    [BUGFIX] productId 없을 때 choose 화면으로 이동

--- src/screen/order/Order.js
+++ src/screen/order/Order.js
@@ -2,19 +2,27 @@ import './Order.css';
 import Header from '../../component/common/Header';
 import Footer from '../../component/common/Footer';
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import Cookie from '../redirect/Cookie';
 import { getProductInfo } from '../../axios/order/Order';
 import OrderTab from '../../component/order/OrderTab';
 import ProductImage from '../../component/order/ProductImage';
 import OrderOptions from '../../component/order/OrderOptions';
+import { CHOOSE } from '../../constants/path';
 
 const Order = () => {
   const [productInfo, setProductInfo] = useState({});
+
+  const navigate = useNavigate();
   const location = useLocation();
 
   const retriveProductData = () => {
-    getProductInfo(location.state.productId).then((res) => {
+    const productId = location.state?.productId;
+    if (!productId) {
+      navigate(CHOOSE);
+      return;
+    }
+    getProductInfo(productId).then((res) => {
       setProductInfo(res.data);
     });
   };
