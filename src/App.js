4653b42ae76c87d0b55d271034dc94b14e718d21
ImKunYoung <lim8667@gmail.com>
2023-11-16 18:00:05 +0900

    [ISSUE-306/BUGFIX] GTM 이벤트 전송 오류 (#307)
    * bugfix(#306): Replaced ReactGA code with GTM script for integration.
    
    * build(#306): Delete react ga library.

--- src/App.js
+++ src/App.js
@@ -41,13 +41,20 @@ import {
 import Faq from './screen/support/Faq';
 import Choose from './screen/order/Choose';
 import { useEffect } from 'react';
-import ReactGA from 'react-ga4';
 
-ReactGA.initialize(process.env.REACT_APP_GA_KEY);
 
 export default function App() {
   useEffect(() => {
-    ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+      var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', process.env.REACT_APP_GA_KEY);
   }, []);
 
   return (
