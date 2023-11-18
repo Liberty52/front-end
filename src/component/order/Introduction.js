b4640e81b732a33e6dac08207c308746ee571a24
2hanbyeol1 <2hanbyeol1@naver.com>
2023-11-18 22:34:49 +0900

    [CSS] Admin page와 css 동일하게 변경

--- src/component/order/Introduction.js
+++ src/component/order/Introduction.js
@@ -1,5 +1,47 @@
 import { useEffect, useRef } from 'react';
-import './Introduction.css';
+import styled from 'styled-components';
+
+const IntroductionContainer = styled.div`
+  text-align: center;
+
+  > p {
+    margin: 0;
+    line-height: 160%;
+  }
+
+  img {
+    width: 60%;
+  }
+
+  button {
+    appearance: auto;
+    text-rendering: auto;
+    color: buttontext;
+    letter-spacing: normal;
+    word-spacing: normal;
+    line-height: normal;
+    text-transform: none;
+    text-indent: 0px;
+    text-shadow: none;
+    display: inline-block;
+    text-align: center;
+    align-items: flex-start;
+    cursor: default;
+    box-sizing: border-box;
+    background-color: buttonface;
+    margin: 0em;
+    padding-block: 1px;
+    padding-inline: 6px;
+    border: 1px outset buttonborder;
+    border-image: initial;
+    border-radius: 3px;
+  }
+
+  ul {
+    list-style: initial;
+  }
+`;
+
 // 상품 소개
 export default function Introduction({ content }) {
   const contentRef = useRef();
@@ -12,5 +54,5 @@ export default function Introduction({ content }) {
     }
   }, [content]);
 
-  return <div className='content-container' ref={contentRef} style={{ textAlign: 'center' }}></div>;
+  return <IntroductionContainer ref={contentRef} />;
 }
