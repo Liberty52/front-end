0ed70693d2eb9b236e7ec442f2978d5ef4916ccc
채희원 <68272931+heeeione@users.noreply.github.com>
2023-12-03 16:10:52 +0900

    chore: License tab -> stockImg tab (#334)

--- src/component/order/editor/icons/index.js
+++ src/component/order/editor/icons/index.js
@@ -3,7 +3,6 @@ import Uploads from './Uploads';
 import SwapHorizontal from './SwapHorizontal';
 import Layers from './Layers';
 import AI from './AI';
-import License from './License';
 import Text from './Text';
 class Icons {
   static Images = Images;
@@ -11,7 +10,6 @@ class Icons {
   static Customize = SwapHorizontal;
   static Layers = Layers;
   static AI = AI;
-  static License = License;
   static Text = Text;
 }
 
